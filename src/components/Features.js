8fa81f0115cf99892bd967838f35255dc57a77e3
Neha Sapre <neha.sapre2114@gmail.com>
2021-07-15 12:25:04 -0500

    Added list of features

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,5 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-<h1>This is the features component</h1>
+function Features() 
+{
+  return (
+        <div>
+            <h3>These are the available features: </h3>
+            <li>Display Stock Info</li>
+            <li>Match with Potential Stocks</li>
+            <li>Stocks You've Matched With</li>
+        </div>
+    )
+}
 
-export default Component;
+export default Features;

761e1d2e342699143551424f5b8a2a6cb12d1d7d
Neha Sapre <neha.sapre2114@gmail.com>
2021-07-15 12:26:12 -0500

    Added Fewatures Component to the Landing Page

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Features from '../components/Features';
 
 function LandingPage() 
 {
   return (
         <div>
             <h1>Hello world</h1>
+            <Features />
         </div>
     )
 }

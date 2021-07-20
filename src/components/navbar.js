237f769876b61d822b939c813d4fbea5627f0a22
jazfv <jazminefuentes@my.unt.edu>
2021-07-20 14:59:57 -0500

    Updated navbar with appropriate names

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,54 +7,31 @@ import {
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
+  } from 'reactstrap';
 
-const Example = (props) => {
+const NavBar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
+      <Navbar color="secondary" light expand="md">
+        <NavbarBrand href="/">Stock Picker</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+              <NavLink href="/Home/">Home</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              <NavLink href="/Match/">Match</NavLink>
             </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
           </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
+            </Collapse>
       </Navbar>
     </div>
   );
 }
 
-export default Example;
+export default NavBar;
--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
 
 function LandingPage() 
 {
   return (
         <div>
-            <h1>Hello world</h1>
+            <NavBar/>
+            <h1>Welcome</h1>
         </div>
     )
 }

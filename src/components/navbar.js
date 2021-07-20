f5af26584591c64a2b376725ab3c3075cd77f8bf
jazfv <81331598+jazfv@users.noreply.github.com>
2021-07-20 15:27:56 -0500

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,7 +9,7 @@ import {
   NavLink,
   } from 'reactstrap';
 
-const NavBar = (props) => {
+function NavBar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);

0cf79a3aa90ae9208731692ecbe0828512e8d0bd
Sophie Hoang <sophie.t.hoang@gmail.com>
2021-07-21 21:12:52 -0500

    Created the explanation component

---
+++ src/components/Explanation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Jumbotron} from 'reactstrap'
+
+function Explanation() {
+    return (
+        <Jumbotron>
+            <h1>Stock Picker</h1>
+            <p>test</p>
+        </Jumbotron>
+    );
+}
+
+export default Explanation;

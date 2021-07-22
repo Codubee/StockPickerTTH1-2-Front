859c8c5fd9a4eb69a9a8281c909844e0abfb07ce
Sophie Hoang <sophie.t.hoang@gmail.com>
2021-07-21 21:59:07 -0500

    Added explanation text

--- src/components/Explanation.js
+++ src/components/Explanation.js
@@ -5,7 +5,15 @@ function Explanation() {
     return (
         <Jumbotron>
             <h1>Stock Picker</h1>
-            <p>test</p>
+            <p>A web application that gives people an easier way to pick a stock.</p>
+            <p>This was created in Summer 2021 by students of Codubee.</p>
+            <h2>How This Works:</h2>
+            <ol>
+                <li>The application will provide you information on a stock</li>
+                <li>Swipe for "yes" or "no"</li>
+                <li>Loop through steps 1 and 2 until you're satisfied</li>
+                <li>Look at your chosen stocks</li>
+            </ol>
         </Jumbotron>
     );
 }

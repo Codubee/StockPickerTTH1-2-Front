1e6eb991d3d4650960253ad900d58ff54f65f0ca
Daniel Jaramillo-Alejo <daniel.jaramillo-alejo@my.unt.edu>
2021-07-22 23:29:29 -0500

    The team component with comments over each important/note worthy component.

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import TeamComponent from '../components/TeamComponent'
 
 function LandingPage() 
 {
   return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
+        <container>
+            <TeamComponent />
+        </container>
     )
 }
 

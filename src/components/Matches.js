cecd1419c2ca1879ec33081b262dcae158ea97db
Sophie Hoang <sophie.t.hoang@gmail.com>
2021-08-02 23:21:07 -0500

    Created the Matches component and added it to the Match page

---
+++ src/components/Matches.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Table } from 'reactstrap'
+
+function Matches() {
+    // companies that the user has matched with
+    const companies = [
+        {companyName: "", symbol: "", price: ""}
+    ]
+}
+
+export default Matches
--- src/views/Match.js
+++ src/views/Match.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Matches from '../components/Matches'
 
 function Match (){
   return(
     // Remove the hello world from here and place your components
-    <h1>Hello world</h1>  
+    //<h1>Hello world</h1>  
+    <Matches/>
   )
 }
 

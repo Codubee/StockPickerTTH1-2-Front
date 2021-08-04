fb2d407087ae853d86410c5a9e361f77a89edd54
Daniel Jaramillo-Alejo <daniel.jaramillo-alejo@my.unt.edu>
2021-08-04 16:37:21 -0500

    Adding the Collapse Component.

---
+++ src/components/Collapse.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+
+
+function CollapseComponent(params) {
+
+const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
+      <Collapse isOpen={isOpen}>
+        <Card>
+          <CardBody>
+          <h1>
+          Hello World.
+          </h1>
+          </CardBody>
+        </Card>
+      </Collapse>
+    </div>
+  );
+}
+
+export default CollapseComponent;
--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Features from '../components/Features';
 import Explanation from '../components/Explanation';
 import {Container} from 'reactstrap'
+import CollapseComponent from '../components/Collapse'
 
 function LandingPage() 
 {
   return (
         <Container>
-            <Explanation/>
+            <CollapseComponent/>
             <Features/>
         </Container>
     )

c6ba487bd8e31db9a345d0ebbfacbd060673f059
Daniel Jaramillo-Alejo <daniel.jaramillo-alejo@my.unt.edu>
2021-08-10 08:39:34 -0500

    comments for collapse funciton

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Collapse, Button, CardBody, Card } from 'reactstrap';
 
-
+//This is the function that will be producing the toggle
 function CollapseComponent(params) {
 
 const [isOpen, setIsOpen] = useState(false);

a473acfd2583fc69f5820836b7efbdfb20841247
codubee <info@codubee.com>
2021-08-19 13:27:23 -0500

    Moved gabes code into collapse

--- src/components/CollapseComponent.js
+++ src/components/CollapseComponent.js
@@ -1,16 +1,33 @@
 import React, { useState } from 'react';
 import { Collapse, Button, CardBody, Card } from 'reactstrap';
 import Matches from './Matches';
+import axios from 'axios'
 
 //This is the function that will be producing the toggle
-class CollapseComponent extends React.Component{
-   constructor(props){
+class CollapseComponent extends React.Component {
+  constructor(props) {
     super(props)
-    this.state = {isOpen:false}
+    this.state = { isOpen: false,matches: [] }
     this.setIsOpen = this.setIsOpen.bind(this)
   }
-  setIsOpen(){
-    this.setState({isOpen:!this.state.isOpen})
+
+  setIsOpen() {
+
+    this.setState({ isOpen: !this.state.isOpen })
+    axios.get("https://stockpicker-tth12-1-back.herokuapp.com/getMatches?userId=99")
+      .then((response) => {
+        console.log(response.data.matches)
+        this.setState({
+
+          // Response.data has the data that came back from the GET http request
+          matches: response.data.matches
+        })
+
+      })
+      .catch((error) => {
+        console.log(error);
+
+      })
   }
 
   render() {
@@ -18,7 +35,7 @@ class CollapseComponent extends React.Component{
       <div className="text-center">
         <Button color="primary" onClick={this.setIsOpen} style={{ marginBottom: '1rem' }}>Toggle</Button>
         <Collapse isOpen={this.state.isOpen}>
-          <Matches/>
+          <Matches matches={this.state.matches} />
         </Collapse>
       </div>
     );
--- src/components/Matches.js
+++ src/components/Matches.js
@@ -1,44 +1,41 @@
 import React from 'react'
 import { Table } from 'reactstrap'
 
-function Matches() {
-    
-    // companies that the user has matched with
-    const companies = [
-        {companyName: "Apple", symbol: "aapl", price: "12.98"}
-    ]
+function Matches(props) {
 
-    return (                
-                /*    Call the map function on the constant created above
-                      Make sure to add a div with a key or else react complains
-                */
 
-                companies.map((companies, id) => (
-                    <div key={id}>
-                        {/* 
-                            Use curly braces to show the data. 
-                            The person being used here comes from the map function created
-                            right above
-                         */}
-                         <Table hover>
-                             <thead>
-                                 <tr>
-                                     <th>Company Name</th>
-                                     <th>Symbol</th>
-                                     <th>Price</th>
-                                 </tr>
-                             </thead>
-                             <tbody>
-                                 <tr>
-                                     <td>{companies.companyName}</td>
-                                     <td>{companies.symbol}</td>
-                                     <td>{companies.price}</td>
-                                 </tr>
-                             </tbody>
-                         </Table>
-                    </div>
-                ))
-    )
+    if (props.matches.length > 0) {
+
+        return (
+            /*    Call the map function on the constant created above
+                  Make sure to add a div with a key or else react complains
+            */
+
+            <Table hover>
+                <thead>
+                    <tr>
+                        <th>Company Name</th>
+                        <th>Symbol</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>{
+                    props.matches.map((companies, id) => (
+
+                        <tbody>
+                            <tr>
+                                <td>{companies.companyName}</td>
+                                <td>{companies.symbol}</td>
+                                <td>{companies.currentPrice}</td>
+                            </tr>
+                        </tbody>
+
+                    ))
+                }
+            </Table>
+        )
+    }
+    else
+        return <h2>No matches yet</h2>
 }
 
 export default Matches

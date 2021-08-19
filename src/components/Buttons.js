063fdd4b12ad97dc3b918f1a8373fae04438b827
Sophie Hoang <sophie.t.hoang@gmail.com>
2021-08-19 02:09:35 -0500

    handled the button click

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,11 +2,34 @@ import React from 'react';
 import { Button } from 'reactstrap'
 
 class Buttons extends React.Component {
+    constructor() {
+        super()
+        //Setting a default value inside of our state object
+        this.state = { company: "Apple" }
+        /*
+            Whenever you are dealing with user interaction like a button click
+            you have to bind the method to the this keyword.
+            If you do not do so, you will not be able to change the state object
+        */
+        this.changeCompany = this.changeCompany.bind(this)
+    }
+
+    /*
+        This is the method that will be called with a button click.
+        It can be named anything you would like as long as it matches
+        what you put with the onClick method in your button html.
+    */
+    changeCompany() {
+        this.setState({ company: "World" })
+    }
+    
     render() {
         return (
             <div>
-                <Button color="success">Yes</Button>
-                <Button color="danger">No</Button>
+                <h1>Hello {this.state.company}</h1>
+
+                <Button color="success" onClick={this.changeCompany}>Yes</Button>
+                <Button color="danger" onClick={this.changeCompany}>No</Button>
             </div>
         )
     }

ddb289dfa7f562e6567164d85204de845e3e7a07
jazfv <jazminefuentes@my.unt.edu>
2021-08-19 11:11:25 -0500

    included delete API in matches component

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -1,44 +1,63 @@
 import React from 'react'
 import { Table } from 'reactstrap'
 
-function Matches() {
-    
-    // companies that the user has matched with
+class Matches extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { Matches: {} };
+        this.deleteApi = this.deleteApi.bind(this);
+    }
+
+    triggerDeleteApi(){
+
+         var queryParam = 1;
+         axios.delete("https://stockpicker-tth12-1-back.herokuapp.com/deleteStock?userId=99&stockId=1"+queryParam)
+
+         .then((response) => {
+             console.log(response.data);
+         })
+
+    }
+    render() {
+         // companies that the user has matched with
     const companies = [
-        {companyName: "Apple", symbol: "aapl", price: "12.98"}
+        { companyName: "Apple", symbol: "aapl", price: "12.98" }
     ]
+        return (
+            /*    Call the map function on the constant created above
+                  Make sure to add a div with a key or else react complains
+            */
 
-    return (                
-                /*    Call the map function on the constant created above
-                      Make sure to add a div with a key or else react complains
-                */
+            companies.map((companies, id) => (
+                <div key={id}>
+                    {/* 
+                    Use curly braces to show the data. 
+                    The person being used here comes from the map function created
+                    right above
+                 */}
+                    <Table hover>
+                        <thead>
+                            <tr>
+                                <th>Company Name</th>
+                                <th>Symbol</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{companies.companyName}</td>
+                                <td>{companies.symbol}</td>
+                                <td>{companies.price}</td>
+                                <td><button onClick={this.triggerDeleteApi}>Delete</button></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+            ))
+        )
+    }
 
-                companies.map((companies, id) => (
-                    <div key={id}>
-                        {/* 
-                            Use curly braces to show the data. 
-                            The person being used here comes from the map function created
-                            right above
-                         */}
-                         <Table hover>
-                             <thead>
-                                 <tr>
-                                     <th>Company Name</th>
-                                     <th>Symbol</th>
-                                     <th>Price</th>
-                                 </tr>
-                             </thead>
-                             <tbody>
-                                 <tr>
-                                     <td>{companies.companyName}</td>
-                                     <td>{companies.symbol}</td>
-                                     <td>{companies.price}</td>
-                                 </tr>
-                             </tbody>
-                         </Table>
-                    </div>
-                ))
-    )
 }
 
 export default Matches
